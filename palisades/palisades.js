37331de63fabb87378facab6360639ae67d35047
aseeman22 <anneseeman27@marlborough.org>
2025-12-05 14:59:43 -0800

    work

--- palisades/palisades.js
+++ palisades/palisades.js
@@ -2,17 +2,35 @@
 
 
 let x = 0;
+let img;
 
-function setup() {
-    createCanvas(1000, 600);
+
+function preload()
+{
+    img = loadImage('Los-Angeles-Neighborhood-Map.jpg');
     
 }
 
+
+function setup() {
+    createCanvas(1000, 600);
+
+    img.resize(width, height);
+}
+
 function draw() {
+    background(0);
+    
+       
+    image(img, 0, 0);
+    
     fill(255);
     textSize(30);
     text('hi annie', width/2, height/2);
 
     ellipse(x, 200, 100, 50);
     x++;
+ 
 }
+
+
