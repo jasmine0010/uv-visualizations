bb5cc4ab9e40e56bd04dd4433a0952e07cb2281d
jasmine <133705110+jasmine0010@users.noreply.github.com>
2025-12-10 00:51:27 -0800

    add forces

--- nsf-grant-visualization/circle.js
+++ nsf-grant-visualization/circle.js
@@ -1,20 +1,35 @@
 class Circle {
-    constructor(grant_id, status, termination_date, project_title, estimated_remaining, max_remaining, division, directorate) {
+    constructor(grant_id, status, termination_date, project_title, abstract, estimated_remaining, min_remaining, max_remaining, division, directorate) {
         this.id = grant_id;
         this.status = status;
         this.term_date = termination_date;
         this.title = project_title;
+        this.abstract = abstract;
         this.remaining = estimated_remaining;
+        this.min_remaining = min_remaining;
         this.max_remaining = max_remaining;
         this.div = division;
         this.dir = directorate;
 
-        this.x = random(width);
-        this.y = random(height);
-        this.r = map(this.remaining, 0, max_remaining, 5, 80);
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(0, 0);
+        this.acc = createVector(0, 0);
+
+        this.r = map(this.remaining, min_remaining, max_remaining, 5, 60);
+    }
+
+    applyForce(f) {
+        this.acc.add(f);
+    }
+
+    update() {
+        this.vel.add(this.acc);
+        this.vel.mult(0.95);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
     }
 
     display() {
-        ellipse(this.x, this.y, this.r, this.r);
+        ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
     }
 }
--- nsf-grant-visualization/sketch.js
+++ nsf-grant-visualization/sketch.js
@@ -1,3 +1,4 @@
+let search_term = "";
 let grants;
 let circles = [];
 
@@ -7,9 +8,13 @@ function preload() {
 
 function setup() {
     createCanvas(1000, 600);
+
+    rectMode(CENTER);
+    textAlign(CENTER);
     
     const grantsArray = Object.values(grants);
     const estimated_remaining = grantsArray.map(g => g.estimated_remaining);
+    const min_remaining = min(estimated_remaining);
     const max_remaining = max(estimated_remaining);
 
     for (let g of grantsArray) {
@@ -18,7 +23,9 @@ function setup() {
             g.status,
             g.termination_date,
             g.project_title,
+            g.abstract,
             g.estimated_remaining,
+            min_remaining,
             max_remaining,
             g.division,
             g.directorate
@@ -27,11 +34,49 @@ function setup() {
 }
 
 function draw() {
+    background(0);
     fill(255);
-    textSize(30);
-    text('hi', width/2, height/2);
-
+    applyRepulsiveForce();
     for (let c of circles) {
-        c.display();
+        c.update();
+        if (search_term != "") {
+            if (c.abstract.includes(search_term)) {
+                c.display();
+            }
+        } else {
+            c.display();
+        }
+    }
+    
+    stroke(0);
+    rect(width*0.9, height*0.9, 50, 50);
+    text("button", width*0.9, height*0.9);
+}
+
+function applyRepulsiveForce() {
+    for (let i = 0; i < circles.length; i++) {
+        for (let j = i + 1; j < circles.length; j++) {
+            let a = circles[i];
+            let b = circles[j];
+
+            let f = p5.Vector.sub(a.pos, b.pos);
+            
+            if (a.r + b.r > f.mag()) {
+                f.setMag(0.2);
+                a.applyForce(f);
+                b.applyForce(f.copy().mult(-1));
+            }
+        }
+    }
+}
+
+function mousePressed() {
+    if (width*0.9 - 25 < mouseX && mouseX < width*0.9 + 25 &&
+        height*0.9 - 25 < mouseY && mouseY < height*0.9 + 25) {
+        if (search_term === "gender") {
+            search_term = "";
+        } else {
+            search_term = "gender";
+        }
     }
 }
