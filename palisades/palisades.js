7a8ef0da7fefffe572af3629c2be9f1f204a8b4d
aseeman22 <anneseeman27@marlborough.org>
2026-01-06 14:12:48 -0800

    work

--- palisades/palisades.js
+++ palisades/palisades.js
@@ -8,6 +8,7 @@ let audio;
 let circles;
 
 
+
 function preload()
 {
     img = loadImage('palisadesStreetMap.jpg');
@@ -17,31 +18,40 @@ function preload()
 
 
 function setup() {
-    createCanvas(1500, 750);
+    createCanvas(1200, 750);
 
-    img.resize(750, 750);
+    img.resize(width, height);
     
-    audio = createAudio('soundEffect.mp3');
+   
     
     circles = [];
-    circles.push(new Marker(250, 250));
-    
+
+    circles.push(new Marker(995, 300, 'audio/EloiseG.mp3')) //Eloise G
+    circles.push(new Marker(660, 480, 'audio/AddieS.mp3'))//Addison S
+    circles.push(new Marker(290, 270, 'audio/EllieL.mp3')) // Ellie L
+    circles.push(new Marker(190, 300, 'audio/CaraH.mp3')) // Cara H
+    circles.push(new Marker(770, 360, 'audio/ChloeS.mp3')) // chloe S
+    circles.push(new Marker(1100, 700, 'audio/EleanorS.mp3'))// eleanor S (altadena)
+    circles.push(new Marker(550, 490, 'audio/EvieT.mp3'))
+    circles.push(new Marker(590, 290, 'audio/TessaHS.mp3'))
+    circles.push(new Marker(350, 320, 'audio/AlexN.mp3'))
 }
+
+
 
 function draw() {
     background(0);
     
        
     image(img, 0, 0);
+    text(mouseX + ' ' + mouseY, 100, 100);
     
-    fill(255);
-    textSize(30);
-    text('hi annie', width/2, height/2);
+    
+   
 
-    ellipse(x, 200, 100, 50);
-    x++;
+   
     
-    ellipse(200, 200, 50, 50);
+  
     
     for(let c of circles)
         {
@@ -53,10 +63,46 @@ function draw() {
 
 function mouseClicked()
 {
-    if(mouseX<= 250 && mouseY >= 150)
+    for(let c of circles)
         {
-              audio.showControls();
+            if(dist(c.x, c.y, mouseX, mouseY) < 20)
+                {
+                    if(!audio)
+                        {
+                            audio = createAudio(c.filename);
+                            audio.showControls();
+                        }
+                }
         }
+  /*if(dist(995, 300, mouseX, mouseY) < 20)
+  {
+      if(!audio)
+          {
+            audio = createAudio('audio/EloiseG.mp3');
+            audio.showControls();
+          }
+  }
+
+  if(dist(660, 480, mouseX, mouseY) < 20)
+      {
+          if(!audio)
+              {
+                  audio = creatAudio('audio/AddisonS.mp3')
+                  audio.showControls();
+              }
+      }
+  if(dist(290, 270, mouseX, mouseY) < 20)
+      {
+          if(!audio)
+              {
+                  audio = createAudio('audio/EllieL.mp3')
+                  audio.showControls();
+              }
+      }
+      */
+  
+    
+
 }
 
 
@@ -67,10 +113,11 @@ function mouseClicked()
 
 class Marker
     {
-        constructor(x,y)
+        constructor(x,y, filename)
         {
             this.x = x;
             this.y = y;
+            this.filename = filename;
         }
         
         display()
